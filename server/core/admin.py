6dd50e0f11943b4ad93a1729b89fcee341f0dc09
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2016-03-24 12:12:17 +0530

    Update admin page.

--- server/core/admin.py
+++ server/core/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
+from django.utils.text import mark_safe
 
 from .models import WebPage, PageScrapeResult
 
 
+class PageScrapeResultAdmin(admin.ModelAdmin):
+    list_display = ["page_name", "updated_on", "link"]
+
+    def page_name(self, obj):
+        return obj.page.feed_name
+
+    def link(self, obj):
+        url = obj.page.url
+        return mark_safe('<a href="{0}" target="_blank">{0}</a>'.format(url))
+
+
 admin.site.register(WebPage)
-admin.site.register(PageScrapeResult)
+admin.site.register(PageScrapeResult, PageScrapeResultAdmin)
