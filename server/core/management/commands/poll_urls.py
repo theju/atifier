74f608c5797e222551d2fbecb55734bea91b04ca
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2016-06-14 19:34:59 +0530

    Calculate the minutes passed correctly.

--- server/core/management/commands/poll_urls.py
+++ server/core/management/commands/poll_urls.py
@@ -16,8 +16,8 @@ class Command(BaseCommand):
         pool = multiprocessing.Pool(multiprocessing.cpu_count())
         today = int(datetime.date.today().strftime("%s"))
         now = datetime.datetime.now()
-        curr_time = float(int(now.strftime("%s")) - now.second)
-        mins_passed = curr_time - today
+        curr_time = int(now.strftime("%s")) - now.second
+        mins_passed = int((curr_time - today) / 60.0)
         for page in WebPage.objects.all():
             if mins_passed % page.interval == 0 or settings.DEBUG:
                 pool.apply_async(scrape_url, (page, ))
